416c6e47c8e206ddab26af955e5226c81ee558a2
Tiago Alves <alvesjtiago@gmail.com>
2017-09-13 15:51:37 +0100

    Add tab bar. Custom tab icons. Start settings and licenses.

---
+++ HNR/Controllers/SettingsTableViewController.swift
@@ -0,0 +1,65 @@
+//
+//  SettingsTableViewController.swift
+//  HNR
+//
+//  Created by Tiago Alves on 13/09/2017.
+//  Copyright © 2017 Tiago Alves. All rights reserved.
+//
+
+import UIKit
+
+class SettingsTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+//    override func numberOfSections(in tableView: UITableView) -> Int {
+//        return 1
+//    }
+//
+//    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        return 1
+//    }
+//
+//    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        let cell = tableView.dequeueReusableCell(withIdentifier: "settingsCell", for: indexPath)
+//
+//        cell.textLabel?.text = "Licenses"
+//
+//        return cell
+//    }
+//    
+//    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+//        switch indexPath.row {
+//        case 0:
+//            self.performSegue(withIdentifier: "licenses", sender: nil)
+//        default:
+//            return
+//        }
+//    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
